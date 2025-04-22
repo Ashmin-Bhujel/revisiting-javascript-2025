cf3329a266b952c7d065efc12e79ce641f511baf
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-04-22 16:53:57 +0545

    update: README.md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 🔥 Revisiting JavaScript in 2025
 
 Just refreshing on JavaScript concepts.
+
+- [x] Basic of JavaScript
+- [ ] Operators in JavaScript
--- main.js
+++
@@ -1,9 +0,0 @@
-const user = {
-  name: "Ashmin Bhujel",
-  age: 21,
-  isBackFromBreak: true,
-};
-
-Object.keys(user).forEach((key, index) => {
-  console.log(`[${index + 1}] - [${key}] - [${user[key]}]`);
-});
